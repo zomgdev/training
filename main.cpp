21aeb64c583fe2d393a7a68981067d8f0f6fedac
cx <sc>
2023-01-25 05:56:12 +0300

    upd

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Mikhail Butalin on 19.09.2022.
+//
+
+#include "main.h"
+#include <iostream>
+
+int main(){
+    unint64_t tt;
+    cout << "Press enter...";
+    cin >> tt;
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by Mikhail Butalin on 19.09.2022.
+//
+#pragma once
+#ifndef TRAINING_MAIN_H
+#define TRAINING_MAIN_H
+
+#endif //TRAINING_MAIN_H
