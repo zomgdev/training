aafd246eee0625829d8dd814a10e69bcf5155a7d
cx <sc>
2023-07-17 23:00:05 +0300

    add server test

---
+++ server01/server01.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Mikhail Butalin on 17.07.2023.
+//
+int main(){
+
+    return 0;
+}
---
+++ server01/server01.h
@@ -0,0 +1,8 @@
+//
+// Created by Mikhail Butalin on 17.07.2023.
+//
+
+#ifndef TRAINING_SERVER01_H
+#define TRAINING_SERVER01_H
+
+#endif //TRAINING_SERVER01_H
