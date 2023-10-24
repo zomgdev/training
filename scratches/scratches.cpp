861d3972e3bf5931c5d71d296cdef7ba4d5554e9
cx <sc>
2023-10-24 07:52:35 +0300

    rebuild

---
+++ scratches/scratches.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    int var01 = 100;
+    std::cout << "var01 = " << var01++ << std::endl;
+    return 0;
+}
---
+++ scratches/scratches.h
@@ -0,0 +1,8 @@
+//
+// Created by Mikhail Butalin on 24.10.2023.
+//
+
+#ifndef TRAINING_SCRATCHES_H
+#define TRAINING_SCRATCHES_H
+
+#endif //TRAINING_SCRATCHES_H
