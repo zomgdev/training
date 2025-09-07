27d9dae30401dac4be96f72c4d94fcdda4825cfb
Mikhail Butalin <mail@zomgdev.com>
2025-09-08 01:13:07 +0300

    Начал компрессию. Пока читка файла.

--- compress01/compress01.c
+++ compress01/compress01.c
@@ -2,45 +2,64 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <time.h>
+#include <string.h>
 
 #include "compress01.h"
 
-int *get_phones_list(int numbers_count){
-
-    int phone_number_value  = 0;
-    int phone_number_size   = 7;
-
-    int *phones_list = malloc(numbers_count*4);
-
-    // инициализация rand
-    srand(time(0));
-    for(int nums_counter = 0; nums_counter < numbers_count; nums_counter++)
-    {
-    phones_list[numbers_count] = rand()%10000000000+1;
-        
-        while (phones_list[numbers_count] < 1000000000 || phones_list[numbers_count] > 9999999999  )
-        {
-            phones_list[numbers_count] = rand()%10000000000+1;
-        }
-     
-        // printf("random number: %d \nsize: %d\n", phone_number_value, sizeof(phone_number_value));
-        printf("%d, %d\n", phones_list[numbers_count], phones_list[numbers_count]);
-    }
-
-    return *phones_list;
-}
 int main(int argc, char **argv)
 {
-    // todo:
     // Постановка задачи.
     // Есть поток, состоящий из байтов, от 0 до 255. Конечный размер не известен, содержимое тоже. 
     // Нужно макимально эффективно сжимать входящий поток без потерь и записывать в файл.
 
-    int number_rate         = 999999;
+    // todo:
+    // Прочитать файл ~1-2GB в память
+    FILE *file;
+    long incoming_file_size = 0;
+    unsigned char *filename = "/opt/testdata/Master.csv";
     
-    // Сначала генерируем тестовый набор данных. Пусть это будет последовательность телефонных номеров, размером 1МБ.
-    int *phones_list = get_phones_list(1000);
+
+    file = fopen(filename, "rb");
+    if (file == NULL){
+        printf("ERROR: Can't open file %s\n", filename);
+        return -1;
+    }
+    
+    fseek(file, 0, SEEK_END);
+    incoming_file_size = ftell(file);
+    rewind(file);
+    printf("File:  %s\n", filename);
+    printf("Size:  %ld bytes\n", incoming_file_size);
+
+    printf("Memory allocation... ");
+    void *file_in_memory = malloc(incoming_file_size + 1);
+    if(!file_in_memory) perror("malloc error");
+    printf("ok. \n");
+
+    printf("Reading file... ");
+    size_t bytes_read = fread(file_in_memory, 1, incoming_file_size, file);
+    if (bytes_read != incoming_file_size) {
+        fprintf(stderr, "Ошибка чтения файла\n");
+        free(file_in_memory);
+        fclose(file);
+        perror("Cant'read file into memory\n");
+        return -1 ;
+    }
+    printf("ok. \n");
+    fclose(file);
+
+    // command line
+    char line[100];
+    printf("cmd >  ");
+    fgets(line, sizeof(line), stdin); // Ожидаем ввод строки
+
+    
+    line[strcspn(line, "\n")] = 0;
+
+    printf("Вы ввели: %s\n", line);
+    free(file_in_memory);
+
 
     //printf("long = %d\n", sizeof(long));
-
+    return 0;
 }
