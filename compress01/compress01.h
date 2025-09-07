c2fa44d4e19430f9b2b2ba8599b36065a0afe21f
Mikhail Butalin <mail@zomgdev.com>
2025-09-08 01:31:13 +0300

    Заготовка для словаря

--- compress01/compress01.h
+++ compress01/compress01.h
@@ -1,2 +1,6 @@
 int PHONE_NUMBER_SIZE        = 7;
 int PHONE_NUMBER_PREFIX_SIZE = 3;
+
+char *left_digits[]  = { 0,  1,  2,  3,  4,  5,  6,   7,   8,   9,  10,  11,  12,  13,  14,  15 } ;
+char *right_digits[] = { 0, 16, 32, 48, 64, 80, 96, 112, 128, 144, 160, 176, 192, 208, 224, 240 };
+
